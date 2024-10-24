adf0b033e258403676d29419cc2db94c8ec55d57
Soumyadip Maity <22053029@kiit.ac.in>
2024-10-24 15:31:33 +0530

    feat: Updated Tailwind config for responsive design and height calculations.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,11 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      screens: {
+        'sm': '768px', // Tablets
+        'xl': '1280px', // 1280px wide desktops
+        '2xl': '1440px', // 1440px wide desktops
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
@@ -19,12 +24,11 @@ const config: Config = {
       height: {
         'screen-minus-20': 'calc(100vh - 240px)',
         'screen-minus-10': 'calc(100vh - 225px)',
-
         // 'screen-minus-30': 'calc(100vh - 135px)',
-
       },
     },
   },
   plugins: [],
 };
+
 export default config;
