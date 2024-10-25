5e210f93c77931598c968593d2876e19ab1d6ead
Soumyadip Maity <22053029@kiit.ac.in>
2024-10-25 20:50:58 +0530

    **feat:** Enhanced layout and visual elements for user testimonials section.

--- src/components/landingpage/Testimonial/testinomial.tsx
+++ src/components/landingpage/Testimonial/testinomial.tsx
@@ -26,7 +26,7 @@ const testimonials = [
 
 const Testimonials: React.FC = () => {
   return (
-    <section className="bg-white py-10 pb-16 sm:px-16">
+    <section className="bg-white py-10 pb-16 lg:px-16">
       <div className="container mx-auto text-center">
         <h2 className="sm:text-5xl text-[38px] font-bold mb-6">What our users say about us?</h2>
         <p className="text-gray-600 mb-12 px-4 font-sans font-semibold text-lg sm:px-16">
@@ -34,7 +34,7 @@ const Testimonials: React.FC = () => {
         Let&rsquo;s hear them and find out what our valued users think about us!!  
         Feel free to add your comment and join our K-prep family!
         </p>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 px-1 sm:px-16">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 px-1 sm:px-16">
           {testimonials.map((testimonial, index) => (
             <div
               key={index}
@@ -44,7 +44,7 @@ const Testimonials: React.FC = () => {
                 <Image src="/k-prep1.png" width={144} height={144} alt="" className='rounded-full sm:w-40' />
               </div>
               <div className="text-left">
-                <h3 className="text-xl mb-1  font-bold">{testimonial.name}</h3>
+                <h3 className="text-xl mb-1 font-bold">{testimonial.name}</h3>
                 <p className="text-sm text-black mb-2">{testimonial.review}</p>
                 <div className="flex">
                   {Array(5)
