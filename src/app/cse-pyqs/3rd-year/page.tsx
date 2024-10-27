38aae7aef7308009ed0a0cdd770caead387d249e
Soumyadip Maity <22053029@kiit.ac.in>
2024-10-27 17:44:03 +0530

    feat: Added new page component ThirdYear with basic layout and content.

---
+++ src/app/cse-pyqs/3rd-year/page.tsx
@@ -0,0 +1,10 @@
+const ThirdYear = () => {
+  return (
+    <div className="w-full h-full bg-[#f8e9f4] overflow-y-auto border rounded-xl border-gray-300">
+      <div className="max-h-screen font-bold p-10">3rd year</div>
+    </div>
+  );
+};
+
+export default ThirdYear;
+
