a5f18906b8bc3b51173a3e3674f76e866003c0a2
Soumyadip Maity <22053029@kiit.ac.in>
2024-10-27 17:45:14 +0530

    feat: Added a React page component for selecting calculator type.

---
+++ src/app/cse-pyqs/calculator/page.tsx
@@ -0,0 +1,16 @@
+const CalculatorPage = () => {
+  return (
+    <div className="w-full h-full bg-[#f8e9f4] overflow-y-auto border rounded-xl border-gray-300">
+      <div className="max-h-screen font-bold p-10">
+        <h1 className="text-center text-4xl">
+          <span className="modal-text">Choose Calculator Type</span>
+        </h1>
+        <div className="mt-10">
+          {/* Add calculator type buttons/cards here */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CalculatorPage;
