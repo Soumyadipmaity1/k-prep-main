8306b5acc0a618bd6d2a0b333ad12a1497ea5b4e
Soumyadip Maity <22053029@kiit.ac.in>
2024-10-27 17:42:54 +0530

    refactor: Refactored CSE-PYQS page with new section containing previous year question resources.

--- src/app/cse-pyqs/page.tsx
+++ src/app/cse-pyqs/page.tsx
@@ -1,15 +1,31 @@
-import Navbar from '@/components/Menubar/Navbar/Navbar'
-import React from 'react'
+import CardResources from "@/components/RecommendedNotes/1st-Year/NotesCard";
 
-function page() {
+const CsePyqsPage = () => {
   return (
-    <div>
-    <Navbar/>
-     <div className='text-5xl flex justify-center items-center h-screen font-extrabold'>
-      Will Be Updated Soon !
+    <div className="w-full sm:h-full h-screen-minus-20 bg-[#f8e9f4] overflow-y-auto border rounded-xl border-gray-300">
+      <div className="bg-[rgb(248,233,244)] p-3 sm:p-10">
+        <h1 className="sm:text-4xl text-xl font-bold mb-5">
+          Previous Year Questions for CSE
+        </h1>
+        <div className="bg-[#f8e9f4]">
+          <h1 className="mb-4 sm:text-2xl text-base pl-1">1st Year PYQs:</h1>
+          <CardResources year={1} />
+        </div>
+        <div className="bg-[#f8e9f4]">
+          <h1 className="my-4 sm:text-2xl text-base pl-1">2nd Year PYQs:</h1>
+          <CardResources year={2} />
+        </div>
+        <div className="bg-[#f8e9f4]">
+          <h1 className="my-4 sm:text-2xl text-base pl-1">3rd Year PYQs:</h1>
+          <CardResources year={3} />
+        </div>
+        <div className="bg-[#f8e9f4]">
+          <h1 className="my-4 sm:text-2xl text-base pl-1">4th Year PYQs:</h1>
+          <CardResources year={4} />
+        </div>
+      </div>
     </div>
-   </div>
-  )
-}
+  );
+};
 
-export default page
+export default CsePyqsPage;
